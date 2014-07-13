5cfbb1bfc77f6dc43f94a500cc97b71a6ada32c7
Jonatan Pedersen <jp@jonatanpedersen.com>
2014-07-13 14:05:19 +0200

    Initial import

---
+++ src/Bocami.Practices.Command.Transactions/TransactionScopeCommandHandlerOfTCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Bocami.Practices.DecoratorPattern;
+using System.Transactions;
+
+namespace Bocami.Practices.Command.Transactions
+{
+    /// <summary>
+    /// Decorates an ICommandHandler of TCommand inside a TransactionScope
+    /// </summary>
+    /// <typeparam name="TCommand"></typeparam>
+    public class TransactionScsopeCommandHandler<TCommand> : ICommandHandler<TCommand>, IDecorator<ICommandHandler<TCommand>>
+        where TCommand : ICommand
+    {
+        private readonly ICommandHandler<TCommand> commandHandler;
+
+        public TransactionScsopeCommandHandler(ICommandHandler<TCommand> commandHandler)
+        {
+            if (commandHandler == null)
+                throw new ArgumentNullException("commandHandler");
+
+            this.commandHandler = commandHandler;
+        }
+
+        public void Handle(TCommand command)
+        {
+            using (var transactionScope = new TransactionScope())
+            {
+                commandHandler.Handle(command);
+
+                transactionScope.Complete();
+            }
+        }
+    }
+}
